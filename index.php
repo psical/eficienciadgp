5f8263784ef4d8897ba072468f5a8fc7e2fae633
psical <148501309+psical@users.noreply.github.com>
2023-10-19 18:13:46 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DGP-Eficiencia</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Creepster&family=Nosifer&display=swap" rel="stylesheet">
+   
+    <video autoplay muted preload loop src="rss/fondo.mp4"></video>
+        
+
+</head>
+<body>
+    <header>
+        <h1>Dirección de Recursos Humanos</h1>
+        <h2>Departamento de Gestión de Personal</h2>
+<br>
+<br>
+        <h2>Bienvenidos</h2>
+        <h2>SEMANA DE LA EFICIENCIA</h2>
+        </header>
+
+        <a class="button" href="datos.php">¿Estas listo?</a>
+
+</body>
+</html>
